7367676f82a628b72a30f3c2e35cbb835fd7dfae
kmsheng <kmsh3ng@gmail.com>
2018-04-01 11:00:35 +0800

    bind MessageService to MessagesComponent

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  // The messageService property must be public because Angular
+  // only binds public component properties to template
+  constructor(public messageService: MessageService) { }
 
   ngOnInit() {
   }
