728a5ed17300029b4912331c425ca5364ef9a4f2
kmsheng <kmsh3ng@gmail.com>
2018-04-01 12:05:36 +0800

    re-style

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
