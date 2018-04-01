c361048581924b434afed075dc4b8ecf8dd1519b
kmsheng <kmsh3ng@gmail.com>
2018-04-01 12:02:00 +0800

    re-style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,9 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ],
   declarations: []
 })
