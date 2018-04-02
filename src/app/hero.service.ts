104919b31e43d56fd6773b18218d498322e85bbb
kmsheng <kmsh3ng@gmail.com>
2018-04-02 11:03:08 +0800

    add updateHero function to HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -37,6 +37,14 @@ export class HeroService {
     );
   }
 
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap(_ => this.log(`update hero id=${hero.id}`),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
+
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
   }
