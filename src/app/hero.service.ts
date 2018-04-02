ec7f0d27536b2b11e8d4d01c8a0b7158eda6dea8
kmsheng <kmsh3ng@gmail.com>
2018-04-02 08:56:10 +0800

    import HttpClient and HttpHeaders in hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
